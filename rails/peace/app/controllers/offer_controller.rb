8976212a081bd487f53101b21f6affcf75101582
Simon <qnstruong@gmail.com>
2014-07-07 12:58:29 +0200

    Love

---
+++ rails/peace/app/controllers/offer_controller.rb
@@ -0,0 +1,12 @@
+class OfferController < ApplicationController
+  def love
+    @presentMoment = Time.now
+  end
+  
+  def integrity
+    
+  end
+  def joyfulplay
+    @oneHour = 1.hour.from_now.localtime
+  end
+end
---
+++ rails/peace/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Peace::Application.config.secret_key_base = 'e4592e36ffc4a8c93b616a1e74379c5159bae5aef878c6e7df125fc96f11a6ecdc0295d6171ad9ea2afe9ce1b76ab38d8d08e6f0a11fd90705e44ec61babce2a'
